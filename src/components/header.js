45c90f84c47d1609b627269b4df649720ed1405f
CodeXRook <lacekitfly@gmail.com>
2019-03-17 10:40:12 -0400

    added link to upload

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,14 @@ export default (props) =>{
     return(
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
 <Link className="navbar-brand" to="/">Mo Feed</Link>
+<ul className="navbar-nav">
+<li className="nav-item">
+<Link className="nav-link" to="/">Home</Link>
+</li>
+<li className="nav-item">
+<Link className="nav-link" to="/upload">Upload</Link>
+</li>
+</ul>
 </nav>
 
     )
