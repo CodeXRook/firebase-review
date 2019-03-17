d2ace777fc47b2788f016ff2947f3e3d17eb3fa2
CodeXRook <lacekitfly@gmail.com>
2019-03-17 09:19:51 -0400

    revise json.stringify

--- src/services/images.js
+++ src/services/images.js
@@ -2,7 +2,9 @@ const ImageService = {} //EMPTY OBJECT
 
 //GETS IMAGES AND PLACES THEM INTO AN ARRAY
 ImageService.init = () => {
-
+    if (localStorage.getItem('images') === null) {
+        localStorage.setItem('images', JSON.stringify([]));
+ }                                    //UNDERSTAND STRINGIFY WITH JSON
 }
 
 export default ImageService;
