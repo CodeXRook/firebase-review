f3212dc5e8c941718bfd06716a132d929f928c62
CodeXRook <lacekitfly@gmail.com>
2019-03-17 09:10:37 -0400

    add header route and path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 
-
+import Header from './component/header';
 
 class App extends Component {
     render () {
         return (
             <HashRouter>
-                
+                <>
+                <Route path='/' component={ Header } />
+
+                </>
            </HashRouter>
         );
     }
