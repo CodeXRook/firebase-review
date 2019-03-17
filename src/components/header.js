4afa794af1b760ba0c2dfff9dd5c9d6997d50f93
CodeXRook <lacekitfly@gmail.com>
2019-03-17 10:37:21 -0400

    added Link

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default (props) =>{
     return(
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
+<Link className="navbar-brand" to="/">Mo Feed</Link>
 </nav>
 
     )
