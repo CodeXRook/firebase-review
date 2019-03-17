c172bbe60b0c19920afe524dac99e78ed7cc62d0
CodeXRook <lacekitfly@gmail.com>
2019-03-17 09:46:29 -0400

    set up success folder

--- src/containers/upload.js
+++ src/containers/upload.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import * as firebase from 'firebase';// * AS FIRE BASE IS IMPORTANT LOWER CASE 
 import ImageService from '../services/images';
+
+
+ // Initialize Firebase
+
+ const config = {
+    apiKey: "AIzaSyDnX-V1IEQDlgNv1XwraQA9jXi3pE8Yomk",
+ }
