81a8ae11cf9d13ab09b5b695c214f951275a18cc
CodeXRook <lacekitfly@gmail.com>
2019-03-17 09:08:44 -0400

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 
+
+
+class App extends Component {
+    render () {
+        return (
+            <HashRouter>
+                
+           </HashRouter>
+        );
+    }
+}
+
 export default App;
