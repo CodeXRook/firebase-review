f17501435e9495b37335bba4c182c89dc4227920
CodeXRook <lacekitfly@gmail.com>
2019-03-17 10:31:18 -0400

    image then timestamp

--- src/components/image.js
+++ src/components/image.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import moment from 'moment';
+
+export default ({ image, timestamp }) => {
+    return (
+        <div className="card bg-dark text-white mb-2" style={{ width: '70%', height: 'auto', marginLeft: 'auto', marginRight: 'auto' }}>
+        <img src={image} className="card-img" alt="..." />
+        </div>
+    )
+}
